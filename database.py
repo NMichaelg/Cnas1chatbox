4be0cd74f14c8959c196efdf2eeebd31692a9131
NMichaelg <107455689+NMichaelg@users.noreply.github.com>
2022-12-17 15:30:31 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
+
 from pymongo import MongoClient
 
 user =  {}
 
 # Establish a connection to mongodb
-client = MongoClient("mongodb+srv://michael:0803@cluster0.wigqva5.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://admin:admin@p2p.pohbbo4.mongodb.net/?retryWrites=true&w=majority")
 db = client.user_info
 log_db = client.user_history
 # Schema 
